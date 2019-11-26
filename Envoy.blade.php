0e74048c11340cafe9f39e44f16483d7346f8d1f
JDBYTE <juandavid92000@gmail.com>
2019-11-26 13:43:48 -0600

    ruta test

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,24 +1,21 @@
-@servers(['aws' => 'ubuntu@3.16.13.217'])
+@servers(['aws' => '-i /home/jd/curso-envoy-aws.pen ubuntu@3.16.13.217'])
 @include('vendor/autoload.php')
+
 
 @setup
 $origin = 'git@github.com:JDBYTE/curso-envoy';
 $branch = isset($branch) ? $branch : 'master';
-$app_dir = '/var/www/html';
-
-
+$app_dir = '/var/www/html/curso-envoy';
 if( !isset($on)){
 throw  new Exception('La variable --on no esta definida');
-
 }
-
 @endsetup
 
 
 @task('git:clone',['on' => $on])
    cd {{ $app_dir }}
    echo "Hemos entrado al directorio /var/www/html";
-   git clone @origin;
+   git clone {{ $origin }} ;
    echo "Repositorio clonado correctamente";
 @endtask
 
@@ -27,4 +24,18 @@ throw  new Exception('La variable --on no esta definida');
 @task('ls',['on' => $on])
 cd {{ $app_dir }}
 ls -la 
+@endtask
+
+
+
+@task('composer:install', ['on' => $on])
+        cd {{ $app_dir }}
+        composer install
+@endtask
+
+
+
+@task('key:generate', ['on' => $on])
+cd {{ $app_dir }}
+php artisan key:generate
 @endtask
